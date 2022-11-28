6a16808a94152694f244d2e28a9f2e9187a08903
codesieg <sieg.heart@6hotmail.fr>
2022-11-28 08:16:12 +0100

    iterate folders

--- readme.md
+++ readme.md
@@ -2,9 +2,14 @@ Configuration google drive :
 Remplir le fichier de conf.exemple dans modules/conf.js 
 
 Pour lancer le serveur : 
-python3 -m http.server 8000
+python3 -m http.server 8000 ou tout autre commande de seerveur installé en local
 
-bien taper dans son navigateur 127.0.0.1:8000 ou chromatique.fr:8000 (si fichier host configuré)
+bien taper dans son navigateur localhost:8000 ou chromatique.fr:8000 (si fichier host configuré)
+
+Lancer la connexion a google
+Executer 
+listfolder
+
 
 liens importants : 
 Api ref : https://developers.google.com/drive/api/v3/reference
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let fileNames = [];
 let idFiles = [];
 let newFolderId;
+let mainNewFolder;
+let firstFolder = false;
+
+let newId;
 
 // Get credentials
 // let apiKey;
@@ -76,20 +80,170 @@ function loadClient() {
       }
     );
 }
+
 // Make sure the client is loaded and sign-in is complete before calling this method.
 function execute() {
-  const file =  gapi.client.drive.files.get({
-    fileId: "1VR3moGu3ePANqR1iwK0iHZeqAvbeetF1",
-  
-  });
-  console.log(file);
+  // we begin to creat the new folder
+  creatFirstFolder();
+}
+
+/**
+ * Create a folder and prints the folder ID
+ * @return{obj} folder Id
+ * */
+let filemetadata;
+async function createFolder(newFolderName) {
+  // Indiquer dans quels dossier créer le dossier de remplacement
+fileMetadata = {
+    name: newFolderName, // TODO : input pour le nom du dossier
+    mimeType: "application/vnd.google-apps.folder",
+    parents: [mainNewFolder],
+  };
+  try {
+    const file = await gapi.client.drive.files.create({
+      resource: fileMetadata,
+      fields: "id",
+    });
+
+    newFolderId = file.result.id;
+    console.log("Folder Id:", file.result.id);
+    // listSubFolders(newFolderId);
+    return file.result.id;
+  } catch (err) {
+    // TODO(developer) - Handle error
+    throw err;
+  }
+}
+/**
+* Copy a folder 
+* 
+* */
+async function copyFolder(parentId, copiedFolderId ) {
+ // Indiquer dans quels dossier créer le dossier de remplacement
+return gapi.client.drive.files.copy({
+  "fileId": copiedFolderId,
+  "resource": {
+    "parents": [ parentId ]
+}
+})
+    .then(function(response) {
+            // Handle the results here (response.result has the parsed body).
+            console.log("Response", response);
+          },
+          function(err) { console.error("Execute error", err); });
+}
+
+// Here function for move old folder to new folder
+async function moveFolder() {
+  // Create new folder
+  //TODO : Needs to make a array with all id sub folders
+
+  for (const idFile of idFiles) {
+    console.log(idFile)
+    try {
+      // Retrieve the existing parents to move and remove
+      const file = await gapi.client.drive.files.get({
+        fileId: "1-AGaKDyle53S6ud5twedEsf39srw71ak",
+        fields: "parents",
+      });
+      console.log(file);
+      const previousParents = file.result.parents;
+      console.log(previousParents);
+
+      previousParents.map(function (parent) {
+        return parent.id;
+      })
+      .join(",");
+      // Move the file to the new folder
+      const files = await gapi.client.drive.files.update({
+        fileId: idFile,
+        addParents: newFolderId,
+        removeParents: previousParents,
+        fields: "id, parents",
+      });
+
+      console.log("Folder Id:", idFile + " déplacé ");
+    } catch (err) {
+      // TODO(developer) - Handle error
+      throw err;
+    }
+  }
+}
+
+async function renameFolder() {
+  try {
+    const getFile = await gapi.client.drive.files.get({
+      fileId: newFolderId,
+    });
+    const updateFile = await gapi.client.drive.files.update({
+      fileId: newFolderId,
+      name: "chroma bis",
+      fields: "id, parents",
+    });
+    console.log("ok");
+
+    const deleteFile = await gapi.client.drive.files.delete({
+      fileId: folderId,
+    });
+    console.log(folderId + " deleted");
+
+    //
+  } catch (error) {
+    console.log("too bad");
+  }
+}
+
+async function regenerateIdFolder() {
+gapi.client.drive.files.generateIds({
+    "count": 1
+  })
+  .then(
+      function(response) {
+        newId =  response.result.ids;
+        console.log(newId["0"]);
+        return newId["0"];
+      }
+  )
+  try {
+    const getFile = await gapi.client.drive.files.get({
+      fileId: newFolderId,
+    });
+    console.log(getFile);
+    const updateFile = await gapi.client.drive.files.update({
+      fileId: newId,
+      name: "chroma bis",
+      fields: "id, parents",
+    });
+    console.log(updateFile);
+    console.log("ok");
+    //
+  } catch (error) {
+    console.log("too bad");
+  }
+}
+
+async function creatFirstFolder() {
+  // creating the new folder 
+  fileMetadata = {
+    name: "new folder 271122", // TODO : input pour le nom du dossier
+    mimeType: "application/vnd.google-apps.folder",
+  };
+  // TODO : explain why firstfolder is needed
+    firstFolder == false;
+    const file = await gapi.client.drive.files.create({
+      resource: fileMetadata,
+      fields: "id",
+    });
+    mainNewFolder = file.result.id;
+    console.log("Folder Id:", file.result.id);
+}
+
+function listFolders() {
   return gapi.client.drive.files
     .list({
-      // supportsAllDrives: true,
       includeItemsFromAllDrives: false,
-      // corpora: "drive",
-      // driveId: "0AIO1nysW2cHkUk9PVA", // ID of drive to connect
       fields: "files(id, name, parents, webViewLink)", // fields we want to include
+      q: `'1E4HZZh1aysmnhpklWkaUzoVUO0vwuE_W' in parents`,
     })
     .then(
       function (response) {
@@ -116,7 +270,9 @@ function execute() {
 
           console.log("Nom : ", file["name"]);
           console.log("Link : ", file["webViewLink"]);
-        }
+          console.log("Id : ", file["id"]);
+          createFolder( file["name"] );
+        } 
         console.log(idFiles);
         console.log("Response", response.result.files);
       },
@@ -126,96 +282,48 @@ function execute() {
     );
 }
 
-/**
- * Create a folder and prints the folder ID
- * @return{obj} folder Id
- * */
-async function createFolder() {
-  // Indiquer dans quels dossier créer le dossier de remplacement
-  const fileMetadata = {
-    // name: fileName,
-    name: "#1 Dossier déplacement pour new lien.", // TODO : input pour le nom du dossier
-    mimeType: "application/vnd.google-apps.folder",
-    // parents: [FOLDER_ID],
-  };
-  try {
-    const file = await gapi.client.drive.files.create({
-      resource: fileMetadata,
-      fields: "id",
-    });
-    newFolderId = file.result.id;
-    console.log("Folder Id:", file.result.id);
+function listSubFolders(subFoldersId) {
+  return gapi.client.drive.files
+  .list({
+    includeItemsFromAllDrives: false,
+    fields: "files(id, name, parents, webViewLink)", // fields we want to include
+    q: `'${subFoldersId}' in parents`,
+  })
+  .then(
+    function (response) {
+      // Handle the results here (response.result has the parsed body).
+      let allFiles = response.result.files;
+      for (const file of allFiles) {
+        fileNames.push(file.name);
+        idFiles.push(file.id);
+        const getUl = document.querySelector("#list-manga-dir");
+        let creatLi = document.createElement("li");
+        let creatA = document.createElement("a");
+        let creatSpan = document.createElement("span");
 
-    return file.result.name;
-  } catch (err) {
-    // TODO(developer) - Handle error
-    throw err;
-  }
-}
+        creatLi.classList.add("list-manga-dir");
 
-// Here function for move old folder to new folder
-async function moveFolder() {
-  // Create new folder
-  // createFolder(newFolderId);
+        creatA.setAttribute("href", file["webViewLink"]);
+        creatA.textContent = file["name"];
+        creatSpan.textContent = " - " + file["id"];
+        getUl.appendChild(creatLi);
+        const getElementLi = getUl.lastChild;
+        console.log(creatA);
+        getElementLi.appendChild(creatA);
+        getElementLi.appendChild(creatSpan);
 
-  //TODO : Needs to make a array with all id sub folders
+        console.log("Nom : ", file["name"]);
+        console.log("Link : ", file["webViewLink"]);
+        createFolder(file["name"]);
 
-  for (const idFile of idFiles) {
-    console.log(idFile)
-    try {
-      // Retrieve the existing parents to move and remove
-      const file = await gapi.client.drive.files.get({
-        fileId: "1-AGaKDyle53S6ud5twedEsf39srw71ak",
-        fields: "parents",
-      });
-      console.log(file);
-      const previousParents = file.result.parents;
-      console.log(previousParents);
-
-      
-      previousParents.map(function (parent) {
-        return parent.id;
-      })
-      .join(",");
-      // Move the file to the new folder
-      const files = await gapi.client.drive.files.update({
-        fileId: idFile,
-        addParents: newFolderId,
-        removeParents: previousParents,
-        fields: "id, parents",
-      });
-
-      console.log("Folder Id:", idFile + " déplacé ");
-    } catch (err) {
-      // TODO(developer) - Handle error
-      throw err;
+      }
+      console.log(idFiles);
+      console.log("Response", response.result.files);
+    },
+    function (err) {
+      console.error("Execute error", err);
     }
-  }
-
-  // TODO : Renomer le nouveau dossier, supprimer l'ancien puis créer le prochain
-}
-
-async function renameFolder() {
-  try {
-    const getFile = await gapi.client.drive.files.get({
-      fileId: newFolderId,
-    });
-    const updateFile = await gapi.client.drive.files.update({
-      fileId: newFolderId,
-      name: "chroma bis",
-      fields: "id, parents",
-    });
-    console.log("ok");
-
-    const deleteFile = await gapi.client.drive.files.delete({
-      fileId: folderId,
-    });
-    console.log(folderId + " deleted");
-
-    //
-  } catch (error) {
-    console.log("too bad");
-  }
+  );
 }
 
 // Besoin supp venir gérer le partage des dossiers et connecter le tout à autocode pour la maj des liens
